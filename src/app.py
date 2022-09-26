b5c206a37c7b7a968bfa2e6a945410c704b9f994
Mylloon <kennel.anri@tutanota.com>
2022-09-26 12:37:07 +0200

    * rename main to app * add index route * add index template * add css stylesheet * add config file

---
+++ src/app.py
@@ -0,0 +1,18 @@
+from routes.index import router as index
+from flask import Flask, redirect
+
+
+app = Flask(__name__, static_url_path="/", static_folder="public")
+app.register_blueprint(index, url_prefix="/index")
+
+
+@app.route('/')
+def root():
+    return redirect("index")
+
+
+class AttributeDict(dict):
+    def __getattr__(self, name):
+        if name in self:
+            return self[name]
+        raise AttributeError(name)
