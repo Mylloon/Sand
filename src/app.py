03122a8617a77cf548f1a525b1b682d139b47cc4
Mylloon <kennel.anri@tutanota.com>
2022-10-07 00:59:40 +0200

    use custom font

--- src/app.py
+++ src/app.py
@@ -1,18 +1,15 @@
-from routes.index import router as index
 from flask import Flask, redirect
+
+from font import init as init_font
+from routes.index import router as index
 
 
 app = Flask(__name__, static_url_path="/", static_folder="public")
 app.register_blueprint(index, url_prefix="/index")
 
+init_font("1.3.0")
+
 
 @app.route("/")
 def root():
     return redirect("index")
-
-
-class AttributeDict(dict):
-    def __getattr__(self, name):
-        if name in self:
-            return self[name]
-        raise AttributeError(name)
---
+++ src/font.py
@@ -0,0 +1,37 @@
+from os import mkdir
+
+from requests import get
+
+
+def exist(path):
+    """Check if file or directory exists"""
+    try:
+        open(path, "r")
+    except FileNotFoundError:
+        return False  # Doesn't exist
+    except IsADirectoryError:
+        return True  # Directory exists
+    else:
+        return True  # File exists
+
+
+def init(version):
+    """ Download font """
+    path = "./src/public/fonts"
+    filename = "RiluRegular.ttf"
+    # If there is filename changes, you should change
+    # the name in the css file too (./src/public/styles/style.css)
+
+    # Check if fonts directory exists
+    if not exist(path):
+        mkdir(path)
+
+    # Download the font file if needed
+    if not exist(f"{path}/{filename}"):
+        # Download the font file
+        file_url = f"https://github.com/alisinisterra/rilu/releases/download/v{version}/{filename}"
+        data = get(file_url).content
+
+        # Save the file
+        with open(f"{path}/{filename}", "wb") as file:
+            file.write(data)
